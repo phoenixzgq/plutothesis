b8f3e1dae1746344d792ee4a6b94dff0299ffcf9
hitluckyfox <hitluckyfox@gmail.com>
2008-03-14 07:42:11 +0000

    提交tags下面的旧版本文件和branches下面的分支文件。

---
+++ PlutoThesis_XeTeX/clean.bat
@@ -0,0 +1,23 @@
+del *.aux /s
+del *.bak /s
+del *.log /s
+
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.toe /s
+
+del *.lof 
+del *.lot
+del *.out 
+
+del *.fen
+del *.ten
+del *.ps
+
+del main.pdf
+del main_xelatex.pdf
+
+
